a943ba207396ab5836cb066d156aa1afaaac47bb
Ivan Ustavitckiy <gita21@mail.ru>
2019-08-23 12:37:53 +0300

    УСЛОЖНЕННОЕ задание

--- lesson01/scrypt.js
+++
@@ -1,5 +0,0 @@
-let money, income, addExpenses, deposit, mission, period;
-
-alert('Привет, Вероника =)');
-
-console.log('Консоль');
---
+++ lesson02/lesson02 hard/script.js
@@ -0,0 +1,21 @@
+//  Усложненное задание
+
+let num = 266219;
+
+num = String(num).split('');
+console.log(num);
+
+let result = num.reduce((a, b) => a * b);
+console.log(result);
+
+result **= 3;
+console.log(result);
+
+
+
+
+
+
+
+
+
