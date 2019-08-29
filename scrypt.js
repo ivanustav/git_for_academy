cb6ad36eb75a93b0c1cb4fec32fc4daca3349dee
Ivan Ustavitckiy <gita21@mail.ru>
2019-08-29 18:35:50 +0300

    lesson06

--- scrypt.js
+++ scrypt.js
@@ -1,5 +1,13 @@
-let arr = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресение'];
+function highlightDays() {
+    let week = ["Понедельник\n", "Вторник\n", "Среда\n", "Четверг\n", "Пятница\n", "Суббота\n", "Воскресенье"]
 
-for (const item of arr) {
-    console.log(item);
+week.forEach((em, i) => {
+    i++;
+    let tagName = (i === 6 || i === 7) ? 'oblique' : 'span';
+    let newEm = document.createElement(tagName);
+    newEm.innerHTML = em;
+    document.body.appendChild(newEm);
+});
 }
+
+console.log(highlightDays());
