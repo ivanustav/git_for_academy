98c1592283d30b4cad26978e4b833cfd4724af1d
Ivan Ustavitckiy <gita21@mail.ru>
2019-08-30 12:50:17 +0300

    Update scrypt.js

--- scrypt.js
+++ scrypt.js
@@ -5,12 +5,12 @@ let week = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
 for (let i = 0; i < week.length; i++) {
     if (i == toDay) {
         if (week[i] == 'сб' || week[i] == 'вс') {
-            document.write(`<p><b><i>${week[i]}</i></b></p>`);
+            document.write(`<p><i>${week[i]}</i></p>`);
         } else {
             document.write(`<p><b>${week[i]}</b></p>`);
         }
     } else if (week[i] == 'сб' || week[i] == 'вс') {
-        document.write(`<p><b>${week[i]}</b></p>`);
+        document.write(`<p><i>${week[i]}</i></p>`);
     } else {
         document.write(`<p>${week[i]}</p>`);
     }
